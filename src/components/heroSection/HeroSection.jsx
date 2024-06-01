d8a9b4e2ecb0d9339a9fc4ef6b314a3604e55e40
ANISH GARG <106441012+anishgarg11@users.noreply.github.com>
2024-06-01 23:36:05 +0530

    Update HeroSection.jsx
    Change Hero Section

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -4,7 +4,7 @@ function HeroSection() {
   return (
     <div>
       <img
-        src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg"
+        src="https://cdn.dribbble.com/userupload/6323400/file/original-96a7c97ff28388207a3dec7d159d9f8d.mp4"
         alt=""
       />
     </div>
