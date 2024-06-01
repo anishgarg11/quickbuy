4d002b1edf3803e4ca19312a4836de29bc570c40
ANISH GARG <106441012+anishgarg11@users.noreply.github.com>
2024-06-01 23:41:17 +0530

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -3,10 +3,9 @@ import React from "react";
 function HeroSection() {
   return (
     <div>
-      <img
-        src="https://cdn.dribbble.com/userupload/6323400/file/original-96a7c97ff28388207a3dec7d159d9f8d.mp4"
-        alt=""
-      />
+     <video>
+  <source src="https://cdn.dribbble.com/userupload/6323400/file/original-96a7c97ff28388207a3dec7d159d9f8d.mp4" type="video/mp4">
+</video>
     </div>
   );
 }
