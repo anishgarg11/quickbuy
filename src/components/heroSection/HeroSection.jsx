857d1094e8f0c6d8e2ee07d351453d9e4610abd7
ANISH GARG <106441012+anishgarg11@users.noreply.github.com>
2024-06-09 01:05:45 +0530

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function HeroSection() {
   return (
-    <div>
- </div>
+    <>
+     <img src="https://t4.ftcdn.net/jpg/03/39/60/67/240_F_339606710_pFQOII8MwyEVqXK5vb4XsIaJr13cipWO.jpg"></img> 
+ </>
   );
 }
 
