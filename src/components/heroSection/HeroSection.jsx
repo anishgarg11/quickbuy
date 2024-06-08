444a2ac66623895c90dfe0a5895aad7625a8e83a
ANISH GARG <106441012+anishgarg11@users.noreply.github.com>
2024-06-09 01:02:58 +0530

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function HeroSection() {
   return (
     <div>
-<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FJ8ZwSuZkCr1JWgo0kXgjG7%2FE-commerce-gradient-illustrations-are-free-and-commercially-available-(Community)%3Fnode-id%3D0-1%26t%3DKi1wHXGsy3nvxOlN-1" allowfullscreen></iframe>    </div>
+ </div>
   );
 }
 
